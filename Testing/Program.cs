487308ac9f6719e5935a17d4e408c6f8f283497f
Carlos Zaldivar Batista <carlos.zaldivar.batista@gmail.com>
2018-04-08 14:53:31 +0200

    Add DisjointSets data structure.

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace Testing
+﻿using System;
+using Common;
+
+namespace Testing
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var boruvka = new Boruvka.Boruvka();
-            var kruskal = new Kruskal.Kruskal();
+
+            DisjointSetsDataStructure sets = new DisjointSetsDataStructure(5);
+
+            sets.Union(0, 1);
+            sets.Union(0, 2);
+            sets.Union(3, 4);
+            sets.Union(2, 3);
+            
+            if (sets.FindSet(0) != sets.FindSet(4))
+                throw new Exception();
         }
     }
 }
