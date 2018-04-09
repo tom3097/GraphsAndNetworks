49fccc0fd8fa345ef85587a34b14192b75975d4b
Carlos Zaldivar Batista <carlos.zaldivar.batista@gmail.com>
2018-04-09 22:53:13 +0200

    Add evaluator.

--- MST/MST/Run.cs
+++ MST/MST/Run.cs
@@ -9,94 +9,63 @@ public class Run
 {
 	public static void Main ()
 	{
-		Console.WriteLine("JOL MADAFAKA");
+		var evaluator = new Evaluator (100);
 
-		Graph g1 = new Graph();
-		g1.GenerateDense(1000);
-		Graph g2 = g1.DeepClone<Graph>();
+		int[] testSizes = {100, 500, 1000, 5000, 10000};
 
-		DateTime startBoruvka = DateTime.Now;
-		Edge[] boruvkaMST = BoruvkaMST.FindMST(g1);
-		DateTime endBoruvka = DateTime.Now;
-
-		DateTime startKruskal = DateTime.Now;
-		Edge[] kruskalMST = KruskalMST.FindMST(g2);
-		DateTime endKruskal = DateTime.Now;
-
-		Console.WriteLine("Boruvka MST:");
-		Array.Sort(boruvkaMST);
-		for (int i = 0; i < boruvkaMST.Length; ++i)
+		foreach (var testSize in testSizes)
 		{
-			Console.WriteLine(boruvkaMST[i].ToString());
+			Console.WriteLine ($"Testing {testSize} node sparse graph.");
+			var results = evaluator.SparseGraphEvaluation (testSize);
+			Console.WriteLine ("Boruvka average time: " + results.BoruvkaAverageTime.TotalMilliseconds);
+			Console.WriteLine ("Kruskal average time: " + results.KruskalAverageTime.TotalMilliseconds);
+
+			Console.WriteLine ($"Testing {testSize} node dense graph.");
+			results = evaluator.DenseGraphEvaluation (testSize);
+			Console.WriteLine ("Boruvka average time: " + results.BoruvkaAverageTime.TotalMilliseconds);
+			Console.WriteLine ("Kruskal average time: " + results.KruskalAverageTime.TotalMilliseconds);
 		}
-
-		Console.WriteLine("Kruska MST:");
-		Array.Sort(kruskalMST);
-		for (int i = 0; i < kruskalMST.Length; ++i)
-		{
-			Console.WriteLine(kruskalMST[i].ToString());
-		}
-
-		TimeSpan boruvkaDiff = endBoruvka - startBoruvka;
-		TimeSpan kruskalDiff = endKruskal - startKruskal;
-
-		Console.WriteLine("Boruvka, czas pracy: " + boruvkaDiff.TotalMilliseconds);
-		Console.WriteLine("Kruskal, czas pracy: " + kruskalDiff.TotalMilliseconds);
-
-		var sparseGraph = new Graph();
-		sparseGraph.GenerateSparse(1000);
-		DummyCorrectnessTests(sparseGraph);
-		var denseGraph = new Graph();
-		denseGraph.GenerateDense(1000);
-		DummyCorrectnessTests(denseGraph);
-		Console.WriteLine("Testy przeszły!");
-		Console.ReadKey();
 	}
 
-	private static void DummyCorrectnessTests(Graph customGraph)
+	private static void DummyCorrectnessTests (Graph customGraph)
 	{
-		var quickGraph = new UndirectedGraph<Vertex, WeightedEdge<Vertex>>();
+		var quickGraph = new UndirectedGraph<Vertex, WeightedEdge<Vertex>> ();
 		var edges = customGraph.E
-			.Select(e => new WeightedEdge<Vertex>(e.U, e.V, e.W))
-			.ToList();
+			.Select (e => new WeightedEdge<Vertex> (e.U, e.V, e.W))
+			.ToList ();
 
-		quickGraph.AddVertexRange(customGraph.V);
-		quickGraph.AddEdgeRange(edges);
+		quickGraph.AddVertexRange (customGraph.V);
+		quickGraph.AddEdgeRange (edges);
 
-		var customBoruvkaMst = BoruvkaMST.FindMST(customGraph);
-		var customKruskalMst = KruskalMST.FindMST(customGraph);
-		var quickGraphMst = quickGraph.MinimumSpanningTreeKruskal<Vertex, WeightedEdge<Vertex>>(e => e.Weight);
+		var customBoruvkaMst = BoruvkaMST.FindMST (customGraph);
+		var customKruskalMst = KruskalMST.FindMST (customGraph);
+		var quickGraphMst = quickGraph.MinimumSpanningTreeKruskal (e => e.Weight);
 
-		if (!(customBoruvkaMst.Length == customKruskalMst.Length && 
-			customKruskalMst.Length == quickGraphMst.Count()))
-		{
-			throw new Exception("Something wrong with the number of edges.");
-		}
+		if (!(customBoruvkaMst.Length == customKruskalMst.Length &&
+		      customKruskalMst.Length == quickGraphMst.Count ()))
+			throw new Exception ("Something wrong with the number of edges.");
 
-		var boruvkaWeight = customBoruvkaMst.Select(e => new BigInteger(e.W)).Aggregate(BigInteger.Add);
-		var kruskalWeight = customKruskalMst.Select(e => new BigInteger(e.W)).Aggregate(BigInteger.Add);
-		var quickGraphWeight = quickGraphMst.Select(e => new BigInteger(e.Weight)).Aggregate(BigInteger.Add);
+		var boruvkaWeight = customBoruvkaMst.Select (e => new BigInteger (e.W)).Aggregate (BigInteger.Add);
+		var kruskalWeight = customKruskalMst.Select (e => new BigInteger (e.W)).Aggregate (BigInteger.Add);
+		var quickGraphWeight = quickGraphMst.Select (e => new BigInteger (e.Weight)).Aggregate (BigInteger.Add);
 
 		if (!(boruvkaWeight == kruskalWeight &&
-			kruskalWeight == quickGraphWeight))
-		{
-			throw new Exception("Something wrong with MST weight.");
-		}
+		      kruskalWeight == quickGraphWeight))
+			throw new Exception ("Something wrong with MST weight.");
 	}
 
-	internal class WeightedEdge<Vertex> : IUndirectedEdge<Vertex>
+	private class WeightedEdge<TVertex> : IUndirectedEdge<TVertex>
 	{
-		public Vertex Source { get; }
-
-		public Vertex Target { get; }
-
-		public int Weight { get; }
-
-		public WeightedEdge(Vertex source, Vertex target, int weight)
+		public WeightedEdge (TVertex source, TVertex target, int weight)
 		{
 			Source = source;
 			Target = target;
 			Weight = weight;
 		}
+
+		public int Weight { get; }
+		public TVertex Source { get; }
+
+		public TVertex Target { get; }
 	}
 }
